e3a73d9107056ab9b2c0994cc8e415e9fbdd7176
Thinh Hoang <dinhthinh.hoang@nxp.com>
2023-11-05 19:47:44 +0100

    Code for the new paper

---
+++ wx/roi.py
@@ -0,0 +1,7 @@
+# Coordinate of KDFW
+KDFW = [-96.973496106, 32.584330996] # lon, lat
+
+# WXR region of interest, check the _plotting_and_roi.ipynb for details
+WX_ROI = [(-103, -90.5), (26.5, 38)] # lon, lat
+
+
