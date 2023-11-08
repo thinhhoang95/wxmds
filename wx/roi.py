1d9d504955a25c8fb17c2bf028eaab882cf20b18
Thinh Hoang <dinhthinh.hoang@nxp.com>
2023-11-08 15:30:53 +0100

    Preprocessing of HRRR data

--- wx/roi.py
+++ wx/roi.py
@@ -2,6 +2,5 @@
 KDFW = [-96.973496106, 32.584330996] # lon, lat
 
 # WXR region of interest, check the _plotting_and_roi.ipynb for details
-WX_ROI = [(-103, -90.5), (26.5, 38)] # lon, lat
-
-
+# WX_ROI = [(-103, -90.5), (26.5, 38)] # lon, lat
+WX_ROI = [(KDFW[0] - 3, KDFW[0] + 3), (KDFW[1] - 3, KDFW[1] + 3)] # lon, lat
